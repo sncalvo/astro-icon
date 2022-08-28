56f95970543b2493b682a5e42da5fe3e2f692f1b
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-08-28 20:55:10 -0300

    Remove svgo

--- packages/core/lib/utils.ts
+++ packages/core/lib/utils.ts
@@ -2,7 +2,6 @@
 import { SPRITESHEET_NAMESPACE } from "./constants";
 import { Props, Optimize } from "./Props";
 import getFromService from "./resolver";
-import { optimize as optimizeSVGNative } from "svgo";
 
 // Adapted from https://github.com/developit/htmlParser
 const splitAttrsTokenizer = /([a-z0-9_\:\-]*)\s*?=\s*?(['"]?)(.*?)\2\s+/gim;
@@ -22,66 +21,66 @@ const splitAttrs = (str) => {
   return res;
 };
 
-function optimizeSvg(
-  contents: string,
-  name: string,
-  options: Optimize
-): string {
-  return optimizeSVGNative(contents, {
-    plugins: [
-      "removeDoctype",
-      "removeXMLProcInst",
-      "removeComments",
-      "removeMetadata",
-      "removeXMLNS",
-      "removeEditorsNSData",
-      "cleanupAttrs",
-      "minifyStyles",
-      "convertStyleToAttrs",
-      {
-        name: "cleanupIDs",
-        params: { prefix: `${SPRITESHEET_NAMESPACE}:${name}` },
-      },
-      "removeRasterImages",
-      "removeUselessDefs",
-      "cleanupNumericValues",
-      "cleanupListOfValues",
-      "convertColors",
-      "removeUnknownsAndDefaults",
-      "removeNonInheritableGroupAttrs",
-      "removeUselessStrokeAndFill",
-      "removeViewBox",
-      "cleanupEnableBackground",
-      "removeHiddenElems",
-      "removeEmptyText",
-      "convertShapeToPath",
-      "moveElemsAttrsToGroup",
-      "moveGroupAttrsToElems",
-      "collapseGroups",
-      "convertPathData",
-      "convertTransform",
-      "removeEmptyAttrs",
-      "removeEmptyContainers",
-      "mergePaths",
-      "removeUnusedNS",
-      "sortAttrs",
-      "removeTitle",
-      "removeDesc",
-      "removeDimensions",
-      "removeStyleElement",
-      "removeScriptElement",
-    ],
-  }).data;
-}
+// function optimizeSvg(
+//   contents: string,
+//   name: string,
+//   options: Optimize
+// ): string {
+//   return optimizeSVGNative(contents, {
+//     plugins: [
+//       "removeDoctype",
+//       "removeXMLProcInst",
+//       "removeComments",
+//       "removeMetadata",
+//       "removeXMLNS",
+//       "removeEditorsNSData",
+//       "cleanupAttrs",
+//       "minifyStyles",
+//       "convertStyleToAttrs",
+//       {
+//         name: "cleanupIDs",
+//         params: { prefix: `${SPRITESHEET_NAMESPACE}:${name}` },
+//       },
+//       "removeRasterImages",
+//       "removeUselessDefs",
+//       "cleanupNumericValues",
+//       "cleanupListOfValues",
+//       "convertColors",
+//       "removeUnknownsAndDefaults",
+//       "removeNonInheritableGroupAttrs",
+//       "removeUselessStrokeAndFill",
+//       "removeViewBox",
+//       "cleanupEnableBackground",
+//       "removeHiddenElems",
+//       "removeEmptyText",
+//       "convertShapeToPath",
+//       "moveElemsAttrsToGroup",
+//       "moveGroupAttrsToElems",
+//       "collapseGroups",
+//       "convertPathData",
+//       "convertTransform",
+//       "removeEmptyAttrs",
+//       "removeEmptyContainers",
+//       "mergePaths",
+//       "removeUnusedNS",
+//       "sortAttrs",
+//       "removeTitle",
+//       "removeDesc",
+//       "removeDimensions",
+//       "removeStyleElement",
+//       "removeScriptElement",
+//     ],
+//   }).data;
+// }
 
 const preprocessCache = new Map();
 export function preprocess(contents: string, name: string, { optimize }) {
   if (preprocessCache.has(contents)) {
     return preprocessCache.get(contents);
   }
-  if (optimize) {
-    contents = optimizeSvg(contents, name, optimize);
-  }
+  // if (optimize) {
+  //   contents = optimizeSvg(contents, name, optimize);
+  // }
   domParserTokenizer.lastIndex = 0;
   let result = contents;
   let token;
